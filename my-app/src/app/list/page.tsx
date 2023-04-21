27236ec324eca7cddccffe2d8d7b8d03d416f394
leesangwon <tkddnjs5613@naver.com>
2023-04-21 22:24:39 +0900

    First Commit

---
+++ my-app/src/app/list/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h4 className="text-center mt-40">상품목록</h4>
+    </div>
+  );
+}
---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h4 className="text-center mt-40">애플후레시</h4>
+      <p className="text-center">by sangwonlee</p>
+    </div>
+  );
+}
